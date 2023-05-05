968752d388e7a03a3f1b63c2259179505077c7ac
Ekaterina Shchukina <43293818+eshchukina@users.noreply.github.com>
2023-05-05 19:58:34 +0300

    New functionality (users)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,71 @@
+<?php return array(
+    'root' => array(
+        'name' => 'eshchukina/todo_api',
+        'pretty_version' => 'v2023-04-07',
+        'version' => '2023.04.07',
+        'reference' => 'cfef59d0a105804f375a12790692e0ab76bd06d1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'container-interop/container-interop' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '^1.2.0',
+            ),
+        ),
+        'eshchukina/todo_api' => array(
+            'pretty_version' => 'v2023-04-07',
+            'version' => '2023.04.07',
+            'reference' => 'cfef59d0a105804f375a12790692e0ab76bd06d1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'laminas/laminas-filter' => array(
+            'pretty_version' => '2.32.x-dev',
+            'version' => '2.32.9999999.9999999-dev',
+            'reference' => 'd68ed259498303fedc9a401e91bf828ca41f474e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laminas/laminas-filter',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'laminas/laminas-servicemanager' => array(
+            'pretty_version' => '3.21.x-dev',
+            'version' => '3.21.9999999.9999999-dev',
+            'reference' => 'e49318be5da41a4477a5e5007c13136f0fc65c62',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laminas/laminas-servicemanager',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'laminas/laminas-stdlib' => array(
+            'pretty_version' => '3.18.x-dev',
+            'version' => '3.18.9999999.9999999-dev',
+            'reference' => '9e6771e6ed80cdadff5a8286a972e4f79607446a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laminas/laminas-stdlib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/container' => array(
+            'pretty_version' => '1.x-dev',
+            'version' => '1.9999999.9999999.9999999-dev',
+            'reference' => '513e0666f7216c7459170d56df27dfcefe1689ea',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/container',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/container-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '^1.0',
+            ),
+        ),
+    ),
+);
